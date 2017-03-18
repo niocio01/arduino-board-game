40f9d691944a3f6b4e964573de886335ca73e7c6
niocio01 <zuber.nico@gmail.com>
2017-03-18 23:51:37 +0100

    speaker treiber begonnen

--- src/Startup.cpp
+++ src/Startup.cpp
@@ -6,26 +6,27 @@
 #include "inttypes.h"
 #include "SpielfeldLed.h"
 #include "GlobalTypes.h"
-
-
-
+#include "SpeakerTreiber.h"
 
 
 static bool ledGestartet;
 static bool lcdGestartet;
 static bool rfidGestartet;
+static bool tonGestartet;
 
 void Startup_check(void)
 {
-  ledGestartet = LedTreiber_Startup();
-  lcdGestartet = LcdTreiber_Startup();
-  rfidGestartet= RfidTreiber_Startup();
+  ledGestartet  = LedTreiber_Startup();
+  lcdGestartet  = LcdTreiber_Startup();
+  rfidGestartet = RfidTreiber_Startup();
+  tonGestartet  = SpeakerTreiber_Startup();
 
-  if (ledGestartet == true and lcdGestartet == true and rfidGestartet == true)
+  if (ledGestartet == true and lcdGestartet == true and rfidGestartet == true and tonGestartet == true)
   {
     LedTreiber_LedSchalten(192, Gruen);
     Messages_ZeigeNachricht(SpielerEins, Startup_OK);
     Messages_ZeigeNachricht(SpielerZwei, Startup_OK);
+    //SpeakerTreiber_playTune();
   }
 
   else
@@ -33,7 +34,6 @@ void Startup_check(void)
     LedTreiber_LedSchalten(192, Rot);
     Messages_ZeigeNachricht(SpielerEins, Startup_RFID_Fail);
     Messages_ZeigeNachricht(SpielerZwei, Startup_RFID_Fail);
-
 
   }
 }
